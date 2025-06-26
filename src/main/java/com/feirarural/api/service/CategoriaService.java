c1d5b5a24d31d87278bbcced8727db0a9bd9f64d
eschoepf <eduardo.schoepf@hotmail.com>
2025-06-26 17:22:02 -0300

    Create a service

---
+++ src/main/java/com/feirarural/api/service/CategoriaService.java
@@ -0,0 +1,30 @@
+package com.feirarural.api.service;
+
+import com.feirarural.api.model.Categoria;
+import com.feirarural.api.repository.CategoriaRepository;
+
+import org.springframework.stereotype.Service;
+import java.util.List;
+
+@Service
+public class CategoriaService {
+    private final CategoriaRepository categoriaRepository;
+
+    public CategoriaService(CategoriaRepository categoriaRepository) {
+        this.categoriaRepository = categoriaRepository;
+    }
+
+    public List<Categoria> listarTodas() {
+        return categoriaRepository.findAll();
+    }
+
+    public Categoria salvar(Categoria categoria) {
+        return categoriaRepository.save(categoria);
+    }
+
+    public Categoria buscarPorId(Long id) {
+        return categoriaRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Categoria não encontrada"));
+    }
+
+}
