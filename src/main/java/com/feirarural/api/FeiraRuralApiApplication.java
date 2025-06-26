875676c99ba97a8b70ee787002fed76442fc30b7
eschoepf <eduardo.schoepf@hotmail.com>
2025-06-26 15:24:49 -0300

    First commit

---
+++ src/main/java/com/feirarural/api/FeiraRuralApiApplication.java
@@ -0,0 +1,13 @@
+package com.feirarural.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeiraRuralApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FeiraRuralApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/feirarural/api/FeiraRuralApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.feirarural.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FeiraRuralApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
