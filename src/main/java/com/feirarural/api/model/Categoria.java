df5d6bf7195afcb7fc84844865d9fe527417f93f
eschoepf <eduardo.schoepf@hotmail.com>
2025-06-30 14:21:16 -0300

    Docker config

--- src/main/java/com/feirarural/api/model/Categoria.java
+++ src/main/java/com/feirarural/api/model/Categoria.java
@@ -17,7 +17,32 @@ public class Categoria {
 
     @Column(nullable = false)
     private String nome;
+
     private String descricao;
 
-    // Getters and Setters
+    // Getters e Setters
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
 }
--- src/main/java/com/feirarural/api/repository/CategoriaRepository.java
+++ src/main/java/com/feirarural/api/repository/CategoriaRepository.java
@@ -1,10 +1,11 @@
 package com.feirarural.api.repository;
 
-import com.feirarural.api.model.Categoria;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import com.feirarural.api.model.Categoria;
+
+@Repository
 public interface CategoriaRepository extends JpaRepository<Categoria, Long> {
     // Additional query methods can be defined here if needed
 
